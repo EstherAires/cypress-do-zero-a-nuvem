5395d1f4fb53ae9179a74cd19c67483ea7cdd1d0
EstherAires <estherlourenco777@gmail.com>
2025-12-28 17:43:06 -0300

    feature: preenche campos obrigatórios no formulário

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -10,5 +10,20 @@ describe("Central de atendimento ao cliente TAT", () => {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
   });
 
+  it("preenche campos obrigatórios no formulário", () => {
+    //nome, sobrenome, email,ajuda
+    cy.get("#firstName").type("Lucas");
+    cy.get("#lastName").type("Silva");
+    cy.get("#email").type("emailteste@gmail.com");
+
+    cy.get("#open-text-area").type(longText, { delay: 0 }); //retira o delay de digitação
+
+    //clicar no botão de enviar
+    cy.get("button[type=submit]").contains("Enviar").click();
+    cy.get(".success")
+      .should("be.visible")
+      .should("contain", "Mensagem enviada com sucesso.");
+  });
+
   // it("", () => {});
 });
